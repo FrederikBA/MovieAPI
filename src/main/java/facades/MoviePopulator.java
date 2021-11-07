15e944878428391bb0748355bfd8ec6d404428ba
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-11-07 15:35:21 +0100

    replace exception @MoviePopulator

--- src/main/java/facades/MoviePopulator.java
+++ src/main/java/facades/MoviePopulator.java
@@ -4,18 +4,19 @@ import dtos.MovieDTO;
 import utils.EMF_Creator;
 
 import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.WebApplicationException;
 
 
 public class MoviePopulator {
 
-    public static void populate() throws InsufficientRatingException {
+    public static void populate() throws WebApplicationException {
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
         MovieFacade facade = MovieFacade.getMovieFacade(emf);
-        
+
         facade.addMovie(new MovieDTO(2021, "TestMovie", "https://www.imdb.com/title/tt1853728/?ref_=nv_sr_srsg_0", 8.4));
     }
 
-    public static void main(String[] args) throws InsufficientRatingException {
+    public static void main(String[] args) {
         populate();
     }
 }
